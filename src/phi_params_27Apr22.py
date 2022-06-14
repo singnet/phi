94a3890c0229ebf23ebb19340fbd4fc66841acc0
Andre Senna <andre.senna@gmail.com>
2022-06-14 09:37:44 -0300

    Fix default parameters and add required folder

--- src/phi_params_27Apr22.py
+++ src/phi_params_27Apr22.py
@@ -1,6 +1,6 @@
-int_len = 12 # This is the desired length of the sliding window used. The code calculates new
+int_len = 20 # This is the desired length of the sliding window used. The code calculates new
              # Phi values across each window, allowing one to see how Phi varies over time.
-num_of_bins = 50 # bins the time-series data for each node into num_of_bins bins
+num_of_bins = 5 # bins the time-series data for each node into num_of_bins bins
 num_of_nodes = 3
 #initializes the number of nodes/features used to calculate Phi. Example graph partitions can be viewed in the files. If Independent Component Analysis (ICA) is turned on, this number will be recomputed on the fly, based upon the dimensional reduction from sk-learn's ICA routine."""
 input_file = '../Sophia_Meditation.csv'
